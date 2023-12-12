42a5bb25c3499481ee136d22fa2b24a318fd6e69
unknown <dejanmaric259@gmail.com>
2023-12-12 17:37:29 +0100

    fetching

--- src/app/_components/Products/Products.js
+++ src/app/_components/Products/Products.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import { ProductBox } from './ProductBox'
 
 export const Products = () => {
+  const [productsData, setProductsData] = useState([]);
+  const URL = 'https://dummyjson.com/products'
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const URL = 'https://dummyjson.com/products';
+        const res = await fetch(URL);
+        const products = await res.json();
+        console.log(products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div>
-        <ProductBox/>
+      {productsData && (<li>
+        
+      </li>)}
     </div>
   )
 }
