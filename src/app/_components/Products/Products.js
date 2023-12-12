1453c8a274098fa4c5cff9dcfa6b4146f750f030
unknown <dejanmaric259@gmail.com>
2023-12-12 17:18:00 +0100

    productss

---
+++ src/app/_components/Products/Products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProductBox } from './ProductBox'
+
+export const Products = () => {
+  return (
+    <div>
+        <ProductBox/>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import { Nav } from './_components'
+import { Nav, Products } from './_components'
 
 export default function Home() {
   return (
    <div>
     <Nav/>
+    <Products/>
    </div>
   )
 }
