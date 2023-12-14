74fe974fb8086a77f05111108dc067262716fcce
unknown <dejanmaric259@gmail.com>
2023-12-14 20:09:30 +0100

    fixing grid

--- src/app/_components/Products/ProductBox/ProductBox.js
+++ src/app/_components/Products/ProductBox/ProductBox.js
@@ -3,8 +3,8 @@ import { CtaButton } from '../../common'
 
 export const ProductBox = ({product}) => {
   return (
-    <div className='rounded-xl max-w-sm  mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4'>
-      <img src={product.images[0]} className='max-w-full h-96 rounded-lg'/>
+    <div className='rounded-xl max-w-sm   mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4'>
+      <img src={product.images[0]} className='max-w-96 h-96 rounded-lg w-96'/>
       <h2 className='mt-4'>{product.title}</h2>
       <h1 className='text-2xl font-bold mt-4'>${product.price}</h1>
       <CtaButton text='Add to Cart'/>
