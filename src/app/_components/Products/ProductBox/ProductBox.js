64789c712f113c1ce6027502d630ddb5a68e4a55
unknown <dejanmaric259@gmail.com>
2023-12-17 20:57:37 +0100

    product box

--- src/app/_components/Products/ProductBox/ProductBox.js
+++ src/app/_components/Products/ProductBox/ProductBox.js
@@ -3,7 +3,7 @@ import { CtaButton } from '../../common'
 
 export const ProductBox = ({product}) => {
   return (
-    <div className='rounded-xl max-w-sm   mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4'>
+    <div className='rounded-xl max-w-sm   mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4 cursor-pointer hover:border-2 border-orange-400'>
       <img src={product.images[0]} className='max-w-96 h-96 rounded-lg w-96'/>
       <h2 className='mt-4'>{product.title}</h2>
       <h1 className='text-2xl font-bold mt-4'>${product.price}</h1>
