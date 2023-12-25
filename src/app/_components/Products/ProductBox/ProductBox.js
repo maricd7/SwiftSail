2ecd2839f69656904b8d0773017ee2941db3cc7f
unknown <dejanmaric259@gmail.com>
2023-12-25 20:33:01 +0100

    adding queries

--- src/app/_components/Products/ProductBox/ProductBox.js
+++ src/app/_components/Products/ProductBox/ProductBox.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { CtaButton } from '../../common'
+import Link from 'next/link';
 
 export const ProductBox = ({product}) => {
 
   const productRef = '/products/' + product.id ; 
 
   return (
-    <a href={productRef} className='rounded-xl max-w-sm   mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4 cursor-pointer hover:border-2 border-orange-400'>
+    <Link  href={{pathname : productRef , query:product}} className='rounded-xl max-w-sm   mx-auto mt-8 bg-white rounded overflow-hidden shadow-lg p-4 cursor-pointer hover:border-2 border-orange-400'>
       <img src={product.images[0]} className='max-w-96 h-96 rounded-lg w-96'/>
       <h2 className='mt-4'>{product.title}</h2>
       <h1 className='text-2xl font-bold mt-4'>${product.price}</h1>
       <CtaButton text='Add to Cart'/>
-    </a>
+    </Link >
   )
 }
