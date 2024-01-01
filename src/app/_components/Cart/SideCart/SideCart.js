f71dfecec607cd06ece7ac8f8ba9be27d238f8f1
unknown <dejanmaric259@gmail.com>
2024-01-01 20:35:00 +0100

    added checkout routing

--- src/app/_components/Cart/SideCart/SideCart.js
+++ src/app/_components/Cart/SideCart/SideCart.js
@@ -2,7 +2,8 @@ import { Icon } from "@iconify/react";
 import React, { useEffect, useState } from "react";
 import { useProductContext } from "@/app/contexts/CartContext";
 import { CartProduct } from "../CartProduct";
-
+import { CtaButton } from "../../common";
+import Link from "next/link";
 export const SideCart = ({ toggleCart }) => {
   const { cart, removeFromCart } = useProductContext();
 
@@ -12,7 +13,7 @@ export const SideCart = ({ toggleCart }) => {
 
   return (
     <div>
-      <div className="h-full w-1/4 bg-white fixed right-0 z-40 mx-0 my-0 p-4">
+      <div className="h-full w-1/4 bg-white fixed right-0 z-40 mx-0 my-0 p-4 top-0">
         <div className="w-full flex justify-end flex-col gap-4">
           <Icon
             className="cursor-pointer"
@@ -24,6 +25,7 @@ export const SideCart = ({ toggleCart }) => {
           />
           {!cart.length && <div>Your Cart is empty</div>}
           {cart && cart.map((item, index) => <CartProduct product={item} onClick={()=>removeFromCart(item.id)} key={index}/>)}
+          <Link href='/checkout'><CtaButton text='Go to checkout'/></Link>
         </div>
       </div>
     </div>
