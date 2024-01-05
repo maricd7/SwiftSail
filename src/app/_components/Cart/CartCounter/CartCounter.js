a0999a31686e22ac554af56b761f171763492e93
unknown <dejanmaric259@gmail.com>
2024-01-05 21:42:37 +0100

    styling cart counter

--- src/app/_components/Cart/CartCounter/CartCounter.js
+++ src/app/_components/Cart/CartCounter/CartCounter.js
@@ -5,6 +5,5 @@ export const CartCounter = () => {
     let {cartCounter} = useProductContext();
     console.log('this is cart counter', cartCounter);
   return (
-    <div>CartCounter</div>
-  )
-}
+     <div className='rounded-full bg-red-500 p-2 w-8 h-8 flex items-center justify-center absolute right-0 top-0'>{cartCounter}</div>
+  )}
