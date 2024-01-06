eeeb2e70a45198b0a6cc27c5af8ee4b31da27407
unknown <dejanmaric259@gmail.com>
2024-01-06 20:25:44 +0100

    total price on checkout

--- src/app/_components/CheckoutProducts/CheckoutProducts.js
+++ src/app/_components/CheckoutProducts/CheckoutProducts.js
@@ -3,9 +3,21 @@ import React, { useContext, useEffect, useState } from "react";
 
 export const CheckoutProducts = () => {
     const [cart,setCart] = useState([])
+    const[total,setTotal] = useState(null);
     useEffect(()=>{
         setCart(JSON.parse(localStorage.getItem('cart')))
-    },[cart])
+    },[])
+
+
+    //total setter
+    useEffect(() => {
+      let sum = 0;
+      cart.forEach((product) => {
+        sum += product.price;
+      });
+      setTotal(sum);
+    }, [cart]);
+
   return (
     <div className='w-96 bg-white rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-8 '>
       <h4 className="font-semibold">Products</h4>
@@ -17,6 +29,10 @@ export const CheckoutProducts = () => {
           </li>
         ))}
       </ul>
+      <div className="w-full flex justify-between align-center mt-4">
+      <p>Total:</p>
+      <span className="font-bold">${total}</span>
+      </div>
     </div>
   );
 };
