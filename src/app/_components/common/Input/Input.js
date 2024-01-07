badbddfa6eda5e5a6db7bd1e9d58f39384d7552c
unknown <dejanmaric259@gmail.com>
2024-01-07 16:25:19 +0100

    adding yup and formik validation

--- src/app/_components/common/Input/Input.js
+++ src/app/_components/common/Input/Input.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-export const Input = ({placeholder,label}) => {
+export const Input = ({ placeholder, label, type, name, onChange, onBlur, value }) => {
   return (
     <div className='flex flex-col py-4'>
-        <label>{label}</label>
-        <input placeholder={placeholder} className='rounded-lg border-2 border-gray-500	 p-2'/>
+      <label>{label}</label>
+      <input
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        className='rounded-lg border-2 border-gray-500 p-2'
+      />
     </div>
-  )
-}
+  );
+};
