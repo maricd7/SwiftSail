901b4a6693ba79f338f10e0769dff8c5b7ba2388
unknown <dejanmaric259@gmail.com>
2024-01-30 15:09:12 +0100

    created category page

--- src/app/_components/Categories/Category/Category.js
+++ src/app/_components/Categories/Category/Category.js
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Category = ({category}) => {
+  const categoryRef = "/categories/" + category;
   return (
-    <div className='bg-gray-700 p-16 rounded-full'>
-      {category}
-    </div>
+    <Link  href={{ pathname: categoryRef}}>
+      <div className='bg-gray-700 p-16 rounded-full'>
+        {category}
+      </div>
+    </Link>
   )
 }
