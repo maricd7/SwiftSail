4e97df953a1a91495d3e14b83932110e4bb5ca8c
unknown <dejanmaric259@gmail.com>
2024-02-17 18:00:42 +0100

    product discount on top box

--- src/app/_components/ProductPage/TopBox/TopBox.js
+++ src/app/_components/ProductPage/TopBox/TopBox.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { CtaButton } from "../../common";
+import ProductPrice from "../../Products/ProductBox/ProductPrice";
 
 export const TopBox = ({ productparams }) => {
   console.log(productparams, 'parametri')
   console.log(productparams, "ovo je kveri");
-  const productName = productparams.get("title");
+  const productName = productparams.get("name");
   const productPrice = productparams.get("price");
   const productDescription = productparams.get("description");
   const productImage = productparams.get('image')
-
+  const productDiscount = productparams.get('discount');
   return (
-    <div className="flex gap-8 mx-auto">
+    <div className="flex gap-8 mx-auto flex-col">
       <img src={productImage} className="w-96 h-96"/>
       <div className="max-w-lg w-lg">
         <h1 className="text-4xl font-bold">{productName}</h1>
         <p>{productDescription}</p>
-        <h2 className="text-2xl">${productPrice}</h2>
+        {productDiscount ? <ProductPrice price={productPrice} discount={productDiscount}/> : <h2 className="text-xl font-bold">${productPrice}</h2>}
         <CtaButton text='Add to cart'/>
       </div>
       <div>
