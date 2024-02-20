1b12ccd1e208b5469fc29aecd159ffc534a405e4
unknown <dejanmaric259@gmail.com>
2024-02-20 19:48:12 +0100

    filter added

--- src/app/_components/Products/Products.js
+++ src/app/_components/Products/Products.js
@@ -7,6 +7,7 @@ import { Loading } from "./loading";
 import { AddedToCart, Heading } from "../common";
 
 export const Products = () => {
+  const [proizvodi,setProizvode] = useState([])
   const { products } = useProductContext();
   const [cartModal, toggleCartModal] = useState(false);
 
@@ -17,12 +18,22 @@ export const Products = () => {
     },3000)
   }
   
+  useEffect(()=>{
+    setProizvode(products); 
+  },[products])
+  function filterProducts(){
+ 
+    const sorted = [...proizvodi].sort((a, b) => a.price - b.price);
+    console.log(sorted);
+    setProizvode(sorted)
+  }
   return (
     <div className="z-10 mx-72 mt-10">
+      <button onClick={()=>filterProducts()}>Filter</button>
       <Heading text='Our international top sellers'/>
-      {cartModal ? <AddedToCart/> : console.log('modal not opened')}
+      {cartModal ? <AddedToCart/> : <></>}
       <ul className="flex flex-wrap gap-4 w-full justify-between items-center">
-        {products.map((product, index) => (
+        {proizvodi.map((product, index) => (
             <ProductBox product={product} key={index}  handleModal={handleModal}/>
           ))}
       </ul>
