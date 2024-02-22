c466e88efd9931b6132cdb16c798dfc23851e2b2
unknown <dejanmaric259@gmail.com>
2024-02-22 10:20:11 +0100

    adding header sections

--- src/app/_components/ShopHeader/ShopHeader.js
+++ src/app/_components/ShopHeader/ShopHeader.js
@@ -1,22 +1,68 @@
 import React from "react";
-import { Heading } from "../common";
 import Image from "next/image";
+import Link from "next/link";
 
 export const ShopHeader = () => {
   return (
     <div className="mt-24 flex mx-72 gap-4 w-full ">
-      <div className="rounded-lg bg-orange-200 w-1/2 h-96 p-8 justify-center items-center flex ">
-        <Heading text="MORE DEALS UP TO 50% DISCOUNT" />
+      <div className="rounded-lg bg-orange-200 w-1/2 p-8 justify-center items-center flex ">
+        <div className="gap-2 flex flex-col ">
+          <h2 className="font-bold text-2xl">MORE DEALS UP TO 50% DISCOUNT</h2>
+          <p className="font-semibold text-slate-700">
+            Explore products with discount to save your money.
+          </p>
+          <Link
+            href="/discounted"
+            className="rounded-full text-white bg-orange-500 py-2 px-8 w-fit"
+          >
+            Explore
+          </Link>
+        </div>
         <Image
           alt="discount product"
-          src="/featured-product.png"
-          width={500}
-          height={500}
+          src="/static/featured-product.png"
+          width={250}
+          height={250}
+          className="right-40"
         />
       </div>
       <div className="flex flex-col w-full gap-4">
-        <div className="rounded-lg bg-red-200 w-1/2 h-full"></div>
-        <div className="rounded-lg bg-blue-200 w-1/2 h-full"></div>
+        <div className="rounded-lg bg-red-200 w-1/2 h-full p-4 justify-center items-center flex">
+          <div className="gap-2 flex flex-col">
+            <h2 className="font-bold text-2xl">VIEW CHEAPEST PRODUCTS TODAY</h2>
+            <Link
+              href="/discounted"
+              className="rounded-full text-white bg-red-500 py-2 px-8 w-fit"
+            >
+              Explore
+            </Link>
+          </div>
+          <Image
+            alt="discount product"
+            src="/static/featured-product.png"
+            width={250}
+            height={250}
+            className="right-40"
+          />
+        </div>
+        <div className="rounded-lg bg-blue-200 w-1/2 h-full p-4 justify-center items-center flex">
+          <div className="gap-2 flex flex-col">
+            <h2 className="font-bold text-2xl">VIEW CHEAPEST PRODUCTS TODAY</h2>
+            <Link
+              href="/discounted"
+              className="rounded-full text-white bg-blue-500 py-2 px-8 w-fit"
+            >
+              Explore
+            </Link>
+          </div>
+          <Image
+            alt="discount product"
+            src="/static/featured-product.png"
+            width={250}
+            height={250}
+            className="right-40"
+          />
+        </div>
       </div>
     </div>
   );
