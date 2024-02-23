7e9fa831a315ea3b7865161d8b9eadb88967f5f1
unknown <dejanmaric259@gmail.com>
2024-02-23 11:48:02 +0100

    creating page for wishlist

--- src/app/_components/Products/ProductBox/WishListBtn.js
+++ src/app/_components/Products/ProductBox/WishListBtn.js
@@ -1,18 +1,39 @@
-'use client'
-import { Icon } from '@iconify/react'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { Icon } from "@iconify/react";
+import React, { useEffect, useState } from "react";
 
-export const WishListBtn = ({product}) => {
-    const [iconColor,setIconColor] = useState('#808080')
-    const [wishList, setWishList] = useState([])
+export const WishListBtn = ({ product }) => {
+  const [iconColor, setIconColor] = useState("#808080");
+  const [wishList, setWishList] = useState([]);
 
-
-    const handleClick = ()=>{
-        setWishList(localStorage.getItem('wishlist'))
-        localStorage.setItem('wishlist', JSON.stringify([...wishList,product]))
-        iconColor === '#808080' ? setIconColor('#FF6961') : setIconColor('#808080');
+  useEffect(() => {
+    const ogWishList = JSON.parse(localStorage.getItem("wishlist"));
+    if (ogWishList) {
+      setWishList(ogWishList);
     }
+  }, []);
+  const handleClick = () => {
+    const existingWishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+    const isProductInWishlist = existingWishlist.some(
+      (item) => item.id === product.id
+    );
+
+    if (isProductInWishlist) {
+      return;
+    }
+    const updatedWishlist = [...existingWishlist, product];
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+
+    setIconColor(iconColor === "#808080" ? "#FF6961" : "#808080");
+  };
   return (
-    <Icon className='absolute left-2 top-2 z-40' onClick={()=>handleClick()} icon="ph:heart-straight-fill" width="24" height="24"  style={{color: iconColor}} />
-  )
-}
+    <Icon
+      className="absolute left-2 top-2 z-40"
+      onClick={() => handleClick()}
+      icon="ph:heart-straight-fill"
+      width="24"
+      height="24"
+      style={{ color: iconColor }}
+    />
+  );
+};
