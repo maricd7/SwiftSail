bb9fea3b0d20eee0cf4b1ff98b6c5b042b01508d
unknown <dejanmaric259@gmail.com>
2024-02-28 20:09:39 +0100

    login works

--- src/app/_components/LoginComponent/LoginComponent.js
+++ src/app/_components/LoginComponent/LoginComponent.js
@@ -1,24 +1,40 @@
-import React, { useState } from "react";
+'use client'
+import React, { useRef, useState } from "react";
 import { CtaButton, Heading, Input } from "../common";
+import supabase from "@/app/supabase";
 
 export default function LoginComponent() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const emailLoginRef = useRef();
+    const passwordLoginRef = useRef();
 
-  async function handleSubmit() {
-    console.log("submited");
-  }
+    async function handleSubmit(e) {
+        e.preventDefault();
+      
+        const email = emailLoginRef.current.value;
+        const password = passwordLoginRef.current.value;
+      
+        const { data, error } = await supabase.auth.signUp({
+          email: email,
+          password: password,
+        });
+      
+        if (error) {
+          console.error("Error signing up:", error.message);
+        } else {
+          console.log("User signed up successfully:", data.user);
+        }
+      }
 
   return (
     <div className="w-full flex justify-center items-center">
       <form
-        onSubmit={handleSubmit()}
+        onSubmit={handleSubmit}
         className="p-16 max-w-lg bg-white rounded-md flex flex-col gap-2"
       >
         <Heading text="Login" />
         <h2>Login in into your swiftsail account for more features.</h2>
-        <Input placeholder="Email" type="text" label="Email" />
-        <Input placeholder="Password" type="password" label="Password" />
+        <Input placeholder="Email" type="text" label="Email" ref={emailLoginRef}/>
+        <Input placeholder="Password" type="password" label="Password" ref={passwordLoginRef}/>
         <CtaButton text="Login" type="submit" />
       </form>
     </div>
--- src/app/_components/common/Input/Input.js
+++ src/app/_components/common/Input/Input.js
@@ -2,7 +2,8 @@
 
 import React from 'react';
 
-export const Input = ({ placeholder, label, type, name, onChange, onBlur, value,errors,touched}) => {
+export const Input = React.forwardRef(({ placeholder, label, type, name, onChange, onBlur, value,errors,touched},ref) => {
+  
   return (
     <div className='flex flex-col py-4 gap-2'>
       <label>{label}</label>
@@ -14,10 +15,11 @@ export const Input = ({ placeholder, label, type, name, onChange, onBlur, value,
         type={type}
         placeholder={placeholder}
         className='rounded-lg border-2 border-gray-500 p-2'
+        ref={ref}
       />
       {errors && touched && (
           <span className='text-red-400'>{errors}</span>
       )}
     </div>
   );
-};
+});
