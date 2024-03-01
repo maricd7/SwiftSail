3930ee00a2e6aa11c3e9e4cf5b5a736425b301f9
unknown <dejanmaric259@gmail.com>
2024-03-01 20:08:22 +0100

    handling signup errors and routing user to home

--- src/app/_components/RegisterComponent/RegisterComponent.js
+++ src/app/_components/RegisterComponent/RegisterComponent.js
@@ -3,27 +3,40 @@ import React, { useRef, useState } from "react";
 import { CtaButton, Heading, Input } from "../common";
 import supabase from "@/app/supabase";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function RegisterComponent() {
     const emailRegisterRef = useRef();
     const passwordRegisterRef = useRef();
-    const displayNameRegisterRef = useRef();
+    const confirmPasswordRef = useRef();
+    const[errorMessage,setErrorMessage] = useState(false)
+    const[errorMessageText,setErrorMessageText]=useState('')
 
+    const router = useRouter()
     async function handleSubmit(e) {
         e.preventDefault();
       
         const email = emailRegisterRef.current.value;
         const password = passwordRegisterRef.current.value;
-        const displayName = displayNameRegisterRef.current;
-
+        const confirmPassword = confirmPasswordRef.current.value;
+        
+        if(password !== confirmPassword ){
+          setErrorMessage(true)
+          return 
+        }else{
+          setErrorMessage(false)
+        }
         const { data, error } = await supabase.auth.signUp({
           email: email,
           password: password,
         });
+
         if (error) {
+          setErrorMessageText(error.message)
           console.error("Error signing up:", error.message);
         } else {
           console.log("User signed up successfully:", data.user);
+          router.push('/')
         }
         
       }
@@ -36,9 +49,11 @@ export default function RegisterComponent() {
       >
         <Heading text="Register" />
         <h2>Register your swiftsail account for more features.</h2>
-        <Input placeholder="Display Name" type="text" label="Display Name" ref={displayNameRegisterRef}/>
         <Input placeholder="Email" type="text" label="Email" ref={emailRegisterRef}/>
         <Input placeholder="Password" type="password" label="Password" ref={passwordRegisterRef}/>
+        <Input placeholder="Confirm Password" type="password" label="Confirm Password" ref={confirmPasswordRef}/>
+        {errorMessage ? <span className="text-red-500 text-sm">Passwords do not match!</span> : <></> }
+        {errorMessage.length>0 ? <span className="text-red-500 text-sm">{errorMessageText}</span>:<></>}
         <CtaButton text="Register" type="submit" />
         <span className="flex gap-2 items-center justify-center mt-4">Already have an account?<Link href='/login' className='text-blue-500'>Login</Link></span>
       </form>
