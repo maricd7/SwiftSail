d68876987dbd89a4a51250dc5be345f491f22318
unknown <dejanmaric259@gmail.com>
2024-03-04 15:52:28 +0100

    checkout page responsive

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -24,7 +24,7 @@ const Checkout = () => {
   },[])
 
   return (
-    <div className='w-full h-cover flex items-start justify-center  gap-8'>
+    <div className='w-full h-cover flex md:flex-row flex-col items-start justify-center  gap-8'>
       <CheckoutForm cart={cart} total_amount={total} quantity={quantity}/>
       <CheckoutProducts cart={cart} total={total}/>
     </div>
