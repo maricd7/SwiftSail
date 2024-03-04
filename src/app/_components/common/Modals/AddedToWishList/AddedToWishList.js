a23d960843908645c36d6698da43cb6885a01021
unknown <dejanmaric259@gmail.com>
2024-03-04 15:37:51 +0100

    responsive modal

--- src/app/_components/common/Modals/AddedToWishList/AddedToWishList.js
+++ src/app/_components/common/Modals/AddedToWishList/AddedToWishList.js
@@ -1,16 +1,15 @@
 'use client'
 import React from 'react';
-import { CtaButton } from '../../CtaButton';
 import { Icon } from '@iconify/react';
 
 export const AddedToWishList = () => {
   return (
-    <div className='flex flex-col fixed top-20 right-72 bg-white p-8 rounded-lg shadow-lg border-l-8 border-red-400	'>
+    <div className='flex flex-col fixed md:top-20 md:right-72 top-16 right-4 bg-white md:p-8 p-4 rounded-lg shadow-lg border-l-8 border-red-400	'>
       <div className='flex  items-center gap-8'>
         <Icon icon="ph:heart-straight-fill" width="24" height="24"  style={{color: '#FF6961'}} />
         <div className='flex flex-col gap-2 '>
-          <span className='text-xl font-bold'>Product added to wishlist!</span>
-          <p>View your favourite products on wishlist page.</p>
+          <span className='md:text-xl text-md font-bold'>Product added to wishlist!</span>
+          <p className='md:text-lg text-sm'>View your favourite products on wishlist page.</p>
         </div>
       </div>
     </div>
