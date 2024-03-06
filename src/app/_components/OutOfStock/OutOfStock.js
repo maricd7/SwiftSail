7630477ada526212c0f0163229b88b6b5fb2b6cd
unknown <dejanmaric259@gmail.com>
2024-03-06 17:21:14 +0100

    hadnling state of wishlist btn

--- src/app/_components/OutOfStock/OutOfStock.js
+++ src/app/_components/OutOfStock/OutOfStock.js
@@ -1,19 +1,31 @@
+'use client'
 import useWishList from "@/app/custom-hooks/useWishList";
-import React from "react";
+import React, { useState } from "react";
 
 export default function OutOfStock({ product,handleRemoveProduct,setWishList,iconColor,setIconColor,wishListModal,setWishListModal,removedWishList,setRemovedWishList}) {
+  
+  const [isInWishList, setIsInWishList] = useState(false);
+
+ 
   function handleClick(){
-    console.log('bror')
+    if (isInWishList) {
+      setIsInWishList(false);
+    }else{
+      setIsInWishList(true)
+    }
+
     useWishList(product,setIconColor,setWishList,wishListModal,removedWishList,setWishListModal,setRemovedWishList)
   }
+  
+
   return (
     <div className="flex flex-col w-full">
       <span className="bg-red-200 text-red-700 py-2 px-4 text-center w-full absolute top-0 z-40 rounded-md">
         Out of Stock
       </span>
       <button onClick={()=>handleClick()} className="rounded-lg w-full flex justify-center mt-4 bg-orange-500 text-white p-4 hover:shadow-[0_3px_10px_rgb(253,126,20,0.2)]">
-        Add to WishList
-      </button>
+        {isInWishList ? "Remove from WishList" : "Add to WishList"}
+      </button> 
     </div>
   );
 }
