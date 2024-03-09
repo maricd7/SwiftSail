1e6a80d7d95d22e1d1709d1f287b296c8f787bfc
unknown <dejanmaric259@gmail.com>
2024-03-09 12:02:18 +0100

    adding modals logic for wishlist

--- src/app/_components/Products/ProductBox/WishListBtn.js
+++ src/app/_components/Products/ProductBox/WishListBtn.js
@@ -2,21 +2,36 @@
 import { useProductContext } from "@/app/contexts/ProductsContext";
 import { Icon } from "@iconify/react";
 import React, { useEffect, useState } from "react";
+import { AddedToWishList, RemovedFromWishList } from "../../common";
 
 export const WishListBtn = ({ product }) => {
   const { addToWishList, removeFromWishlist, wishlist } = useProductContext();
-  
+  const [wishListModal,setWishListModal] = useState(false)
+  const [removedWishList,setRemovedWishList]=useState(false)
   const isProductInWishlist = wishlist.some(wish => wish.id === product.id);
   const wishlistIcon = isProductInWishlist ? '#FF6961' : '#808080';
 
   function handleClick() {
     if (isProductInWishlist) {
       removeFromWishlist(product.id);
+      handleRemove()
     } else {
       addToWishList(product);
+      handleModal()
     }
   }
-
+  function handleModal() {
+    setWishListModal(!wishListModal);
+    setTimeout(() => {
+      setWishListModal(false);
+    }, 1500);
+  }
+  function handleRemove() {
+    setRemovedWishList(!wishListModal);
+    setTimeout(() => {
+      setRemovedWishList(false);
+    }, 1500);
+  }
   return (
     <div className="z-20">
       <Icon
@@ -27,6 +42,8 @@ export const WishListBtn = ({ product }) => {
         height="24"
         style={{ color: wishlistIcon }}
       />
+      <AddedToWishList/>
+      <RemovedFromWishList/>
     </div>
   );
 };
