960dd237294fb1e63149d68393c978e2de8afb40
unknown <dejanmaric259@gmail.com>
2024-03-20 13:34:29 +0100

    newsletter toggler

--- src/app/_components/common/Modals/NewsLetter/NewsLetter.js
+++ src/app/_components/common/Modals/NewsLetter/NewsLetter.js
@@ -3,17 +3,21 @@ import React from 'react'
 import { Input } from '../../Input'
 import { CtaButton } from '../../CtaButton'
 import { Icon } from '@iconify/react'
+import Image from 'next/image'
 
-export default function NewsLetter() {
+export default function NewsLetter({setNewsLetter}) {
   return (
-    <div className='absolute p-32 rounded-lg z-50 bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg'>
-        <Icon className='absolute top-4 right-4 cursor-pointer' icon="carbon:close" width="24" height="24"  style={{color: '#000'}} />
+    <div className='absolute rounded-lg z-50 bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg flex gap-4'>
+        <Image width={500} height={500} alt='Newsletter image' src='https://images.unsplash.com/photo-1573855619003-97b4799dcd8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <div className='p-16'>
+      <Icon onClick={()=>{setNewsLetter(false)}} className='absolute top-4 right-4 cursor-pointer' icon="carbon:close" width="24" height="24"  style={{color: '#000'}} />
         <h2 className='text-2xl font-bold text-center'>Keep up with latest discounts and offers.</h2>
         <p className='text-lg text-center'>Unlock exclusive deals and updates! Subscribe now for the latest trends and promotions straight to your inbox.</p>
         <div>
             <Input placeholder='Your email'/>
             <CtaButton text='Subscribe'/>
         </div>
+      </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,17 +9,21 @@ import Disclaimer from "./_components/Disclaimer/Disclaimer";
 import NewsLetter from "./_components/common/Modals/NewsLetter/NewsLetter";
 
 export default function Home() {
+  const [newsLetter,setNewsLetter] = useState(false)
   const [cartOpened, setCartOpened] = useState(false);
   function toggleCart() {
     setCartOpened(!cartOpened);
   }
+  setTimeout(()=>{
+    setNewsLetter(true)
+  },3000)
   return (
     <div>
       <ProductContextProvider>
       <Nav/>
        <div className="mt-20">
         <ShopHeader/>
-        <NewsLetter/>
+        {newsLetter ? <NewsLetter setNewsLetter={setNewsLetter}/> : <></>}
        {/* <Slider/> */}
         <Categories/>
         <Products />
